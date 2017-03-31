8ccaf291b339ed0f0e6398ad5c453a8b7ad5784a
Neeraj <8patidarneeraj@gmail.com>
2017-03-31 23:45:45 +0530

    final commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Udacity Build It Bigger project
+===================================
+
+Project 4 of Udacity's Android Nanodegree program.
+
+
+Objective
+--------------
+Learn Gradle and automate building tasks.
---
+++ joker/src/main/java/com/andressantibanez/Joker.java
@@ -0,0 +1,13 @@
+package com.andressantibanez;
+
+public class Joker {
+
+    public Joker() {
+
+    }
+
+    public String getJoke() {
+        return "Team work is important; it helps to put the blame on someone else.";
+    }
+
+}
